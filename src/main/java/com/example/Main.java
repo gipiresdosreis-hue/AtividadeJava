0808d57cad7d9a5438498f513ab8c7daf4a27322
50033949859 <gipiresdosreis@gmail.com>
2026-09-14 15:37:44 -0300

    feat: Atividade 5 - caixa de supermercado

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,29 +1,43 @@
+
 package com.example;
 
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
 
         Scanner entrada = new Scanner(System.in);
 
-        int qtdCliente = 0;
+        double valorProduto = 0;
+        double soma = 0;
+        int contador = 0;
+        String continuar;
 
-        int nota = 0;
-        System.out.print("Informe a nota da avaliação : ");
-       
-       qtdCliente = entrada.nextInt();
+        do {
 
+            System.out.print("Informe o valor do produto: ");
 
-        while (qtdCliente < 10) {
-            qtdCliente++;
-            System.out.println("Cliente " + qtdCliente + " informe sua nota");
-            nota = entrada.nextInt();
-            
+            valorProduto = entrada.nextDouble();
 
-        }
-        System.out.println("Quantidade de clientes que avaliaram : " + qtdCliente);
+            soma = soma + valorProduto;
 
-        
+            contador++;
+
+            System.out.print("Deseja continuar? (s/n): ");
+
+            entrada.nextLine(); // Limpa o buffer do Scanner
+
+            continuar = entrada.nextLine();
+
+        } while (continuar.equalsIgnoreCase("s")
+                || continuar.equalsIgnoreCase("sim"));
+
+        System.out.println("Voce comprou " + contador + " itens.");
+        System.out.println("A soma dos produtos é: " + soma);
+
+        entrada.close();
     }
 }
+
+
